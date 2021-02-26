4325e31780f0ce043f005d764b5ef5b6dea0ce51
mark-nick-o <74973243+mark-nick-o@users.noreply.github.com>
2021-02-26 18:06:13 +0000

    Update makeTestPics.sh

--- makeTestPics.sh
+++ makeTestPics.sh
@@ -1,3 +1,4 @@
 :
 yourFolder=anumal/testanimal
+num=1
 for i in `ls /mnt/c/linuxmirror/$yourFolder`; do echo $i; fil=`echo $i | awk '-F.' '{ print $1 }'`; ext=`echo $i | awk '-F.' '{print $2}'`;cp /mnt/c/linuxmirror/$yourFolder/$i /mnt/c/linuxmirror/$yourFolder/$num.$ext; num=`expr $num + 1`; done
